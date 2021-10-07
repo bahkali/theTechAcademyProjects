29fbab7931f1918261be6fd9ec587e8b5ffb3526
kaly <kalimamadou@gmail.com>
2021-10-06 23:41:41 -0700

    Practicing javascript basic main.js with manupulation on string and javascript expression

--- JavaScript-Projects/Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
@@ -1,1 +1,10 @@
-alert("Hello from main.js file :/");
+window.alert("Hello from main.js file :/\n");
+document.write("What is this?\n");
+
+var A = "This is a string";
+document.write(A);
+// Concatenation assignment
+// document.write("" + "" + "");
+var blues = "I have the blues";
+var blues = blues.fontcolor("blue");
+document.write(blues);
