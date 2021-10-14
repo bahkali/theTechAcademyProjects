91d5412138cc2ad189ba9ee4d8fdb0586e4a4226
kaly <kalimamadou@gmail.com>
2021-10-13 22:47:43 -0700

    add comment to main js on basic javascript project 1

--- JavaScript-Projects/Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
@@ -1,10 +1,11 @@
+// Using window alert to display a message
 window.alert("Hello from main.js file :/\n");
+// Document write manipulate the window and display text
 document.write("What is this?\n");
-
+// Assign value to variable
 var A = "This is a string";
 document.write(A);
-// Concatenation assignment
-// document.write("" + "" + "");
+// Using Fontcolor to change text color
 var blues = "I have the blues";
 var blues = blues.fontcolor("blue");
 document.write(blues);
