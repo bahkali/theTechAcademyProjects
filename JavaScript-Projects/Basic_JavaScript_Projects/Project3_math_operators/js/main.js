215b27daf8ce89b1ff6547eb17e6fb461addb506
kaly <kalimamadou@gmail.com>
2021-10-17 13:35:38 -0700

    Project 3 Javascript Math Add remaining operation function and display result

--- JavaScript-Projects/Basic_JavaScript_Projects/Project3_math_operators/js/main.js
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project3_math_operators/js/main.js
@@ -2,3 +2,17 @@ function add_function() {
   let additon = 2 + 2;
   document.getElementById("mathP").innerHTML = "2 + 2= " + additon;
 }
+
+function subtract_function() {
+  let subtract = 5 - 2;
+  document.getElementById("mathS").innerHTML = "5 + 2= " + subtract;
+}
+
+function multiplication_function() {
+  let multiplication = 6 * 8;
+  document.getElementById("mathM").innerHTML = "6 x 8= " + multiplication;
+}
+function division_function() {
+  let division = 48 / 6;
+  document.getElementById("mathD").innerHTML = "48 / 6= " + division;
+}
