1bb668336c06b13d63bf317a40585c686bffe5eb
kaly <kalimamadou@gmail.com>
2021-10-17 13:16:15 -0700

    Project 2 javascript function: Call function and using concatenation text manipulation:

--- JavaScript-Projects/Basic_JavaScript_Projects/Project2_functions/js/main.js
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project2_functions/js/main.js
@@ -1,6 +1,12 @@
 function myFunction() {
-  sentence = "This text has change.";
+  var sentence = "This text has change.";
   document.getElementById("paragraph").innerHTML = sentence;
+}
+
+function concatFunction() {
+  var sentence = "This text has concatenated with this = ";
+  sentence += " I love conding all day.";
+  document.getElementById("concatenate").innerHTML = sentence;
 }
 
 // document.getElementById('btn').onclick({
