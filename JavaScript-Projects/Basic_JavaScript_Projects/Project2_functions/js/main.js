17e96d79bad7d6a467833b27d32d22780dc20e06
kaly <kalimamadou@gmail.com>
2021-10-17 13:04:27 -0700

    Project2 Javascript function: Work on onclick function

---
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project2_functions/js/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  sentence = "This text has change.";
+  document.getElementById("paragraph").innerHTML = sentence;
+}
+
+// document.getElementById('btn').onclick({
+//     sentence = "This text has change."
+//   document.getElementById("paragraph").innerHTML = sentence
+// });
