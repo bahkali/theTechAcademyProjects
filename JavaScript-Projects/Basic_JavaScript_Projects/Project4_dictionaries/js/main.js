7962beebd2c8fe7d3c827070a758b506d4a0c040
kaly <kalimamadou@gmail.com>
2021-10-18 17:23:29 -0700

    Project 4 Javascript Dictionary Using a function to call data of Dictionary

---
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project4_dictionaries/js/main.js
@@ -0,0 +1,10 @@
+function my_dictionary() {
+  var animal = {
+    species: "Dog",
+    color: "Black",
+    Breed: "Labrador",
+    Age: 5,
+    Sound: "Bark!",
+  };
+  document.getElementById("Dictionary").innerHTML = animal.Sound;
+}
