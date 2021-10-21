7881e5178845a8aae94139cd140f90b6f9d37cbc
kaly <kalimamadou@gmail.com>
2021-10-21 14:22:00 -0700

    Project 5 Comparison Type Coercion: Display infinity and - infinity on the page :

--- JavaScript-Projects/Basic_JavaScript_Projects/Project5_comprarisons_type_coercion/js/main.js
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project5_comprarisons_type_coercion/js/main.js
@@ -1,5 +1,8 @@
 document.write("234" + 56);
+document.write("\n");
 document.write(typeof "Hello");
 function my_function() {
   document.getElementById("Test").innerHTML = isNaN("This is a string");
+  document.getElementById("infinity").innerHTML = 2e310;
+  document.getElementById("Ninfinity").innerHTML = -3e310;
 }
