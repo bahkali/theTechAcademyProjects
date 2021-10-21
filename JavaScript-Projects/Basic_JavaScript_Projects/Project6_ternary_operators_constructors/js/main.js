71768559d5e2f2c65c959e7a90833e54fe5a37fb
kaly <kalimamadou@gmail.com>
2021-10-21 15:46:13 -0700

    Project 6 Ternary Operators and Constructors: Using function to build a constructor function using keyword this

--- JavaScript-Projects/Basic_JavaScript_Projects/Project6_ternary_operators_constructors/js/main.js
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project6_ternary_operators_constructors/js/main.js
@@ -13,3 +13,23 @@ function Vote_function() {
   Can_vote = Vote < 18 ? "You are not old enough to vote" : "You can vote!";
   document.getElementById("Vote").innerHTML = Can_vote;
 }
+
+// Using This keyword to build an constructor function
+function Vehicle(Make, Model, Year, Color) {
+  this.Vehicle_Make = Make;
+  this.Vehicle_Model = Model;
+  this.Vehicle_Year = Year;
+  this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function my_function() {
+  document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " +
+    Erik.Vehicle_Color +
+    "-colored " +
+    Erik.Vehicle_Model +
+    " manufactured in " +
+    Erik.Vehicle_Year;
+}
