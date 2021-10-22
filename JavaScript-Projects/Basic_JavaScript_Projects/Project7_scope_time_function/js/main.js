204a083709a984ca719335b980244b105fea7df4
kaly <kalimamadou@gmail.com>
2021-10-22 14:56:52 -0700

    Project 7 Scope Time Function: Write a time time function using else if statement and display message base on time of the day

--- JavaScript-Projects/Basic_JavaScript_Projects/Project7_scope_time_function/js/main.js
+++ JavaScript-Projects/Basic_JavaScript_Projects/Project7_scope_time_function/js/main.js
@@ -36,3 +36,17 @@ function Age_function() {
   }
   document.getElementById("how_old_are_you").innerHTML = Vote;
 }
+
+// Else If Statemet
+function Time_function() {
+  var Time = new Date().getHours();
+  var Reply;
+  if (Time < 12 == Time > 0) {
+    Reply = "It is morning time!";
+  } else if (Time >= 12 == Time < 18) {
+    Reply = "It is afternoon.";
+  } else {
+    Reply = " It is evening time.";
+  }
+  document.getElementById("Time_of_day").innerHTML = Reply;
+}
